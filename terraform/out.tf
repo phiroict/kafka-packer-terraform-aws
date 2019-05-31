c94b625a6fb805a44210defb41e59ec80224e36f
Philip Rodrigues <phiroict@gmail.com>
2019-05-31 17:47:24 +1200

    Added concourse.

--- terraform/out.tf
+++ terraform/out.tf
@@ -1,6 +1,13 @@
 output "bastion_ip" {
   value = "Bastion IP is ${aws_eip.kafka_ip_address.public_ip}"
 }
+output "private_brokers0" {
+  value = "Private brokers ${aws_instance.kafka_instance_private_brokers[0].private_dns}"
+}
+output "private_brokers1" {
+  value = "Private brokers ${aws_instance.kafka_instance_private_brokers[1].private_dns}"
+}
+
 
 output "private_dns" {
   value = "DNS: ${aws_instance.kafka_instance_public_broker.private_dns}"
