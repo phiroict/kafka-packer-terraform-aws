2cab234fe8b7937949190021af9ebffed0cd54df
Philip Rodrigues <phiroict@gmail.com>
2019-11-03 18:09:50 +1300

    Split zookeeper and kafka instances.

--- terraform_state/creates3.tf
+++ terraform_state/creates3.tf
@@ -7,3 +7,10 @@ resource "aws_s3_bucket" "state_bucket" {
     Environment = "Prod"
   }
 }
+
+resource "aws_s3_bucket_object" "state_folder" {
+  bucket = "${aws_s3_bucket.state_bucket.id}"
+  acl    = "private"
+  key    = "kafka-packer-terraform-aws/"
+  source = "/dev/null"
+}
